c944dd8b4f16feb52f9628bd26c0641066afc039
Misael Corvera <mcmalfaro@hotmail.com>
2019-08-05 17:50:31 -0700

    init commit

---
+++ src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../Style/home.css';
+
+class Home extends React.Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h1>hELLO hOME</h1>
+            </div>
+         );
+    }
+}
+ 
+export default Home;
