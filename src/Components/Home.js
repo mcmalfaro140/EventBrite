384633852da4baaf66d22a8e45487e1253fda377
mcmalfaro140 <mcmalfaro@hotmail.com>
2019-11-21 14:07:22 -0800

    added suspense

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import '../Style/home.css';
-import EventList from '../Components/EventList'
-
-
+import { Suspense, lazy } from 'react';
+const EventList = lazy(() => import('../Components/EventList'));
 class Home extends React.Component {
     state = { 
         myEvents: []
@@ -28,7 +27,10 @@ class Home extends React.Component {
                     <img src={require('../img/cover.jpg')}></img>
                 </div>
                 <div className="slogan">Live your best life</div>
-                <EventList myEvents={this.state.myEvents}></EventList>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <EventList myEvents={this.state.myEvents}></EventList>
+                </Suspense>
+                
                 <div className="footer">
                     <div className="question">Want to create an <span style={{color:'yellow'}}>event</span>?</div>
                     <div >Learn more <i class="fas fa-chevron-circle-right"></i></div>
